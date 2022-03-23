ef6da4a9f85607f94bd8540f6ac9b80d6132452f
Timmy Bergvall <79316874+TimmyBergvall@users.noreply.github.com>
2022-03-23 15:54:05 +0100

    Lab4

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 1337;
+
+
+app.use(express.static("public"));
+
+
+app.listen(PORT, () => {
+    console.log("You are live on portnumber " + PORT);
+})
